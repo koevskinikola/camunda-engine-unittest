1ad982c09ef08b389b073356291fa499489db619
Nikola Koevski <nikola.koevski@camunda.com>
2019-04-25 14:23:14 +0200

    create test case for concurrent variable creation

--- src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
@@ -16,40 +16,85 @@
  */
 package org.camunda.bpm.unittest;
 
-import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.OptimisticLockingException;
+import org.camunda.bpm.engine.impl.ProcessEngineLogger;
+import org.camunda.bpm.engine.impl.cmd.SetTaskVariablesCmd;
+import org.camunda.bpm.engine.impl.test.PluggableProcessEngineTestCase;
 import org.camunda.bpm.engine.test.Deployment;
-import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.slf4j.Logger;
 
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
-
-import org.junit.Rule;
-import org.junit.Test;
+import java.util.Collections;
 
 /**
  * @author Daniel Meyer
  * @author Martin Schimak
  */
-public class SimpleTestCase {
+public class SimpleTestCase extends PluggableProcessEngineTestCase {
 
-  @Rule
-  public ProcessEngineRule rule = new ProcessEngineRule();
+  private static Logger LOG = ProcessEngineLogger.TEST_LOGGER.getLogger();
 
-  @Test
-  @Deployment(resources = {"testProcess.bpmn"})
-  public void shouldExecuteProcess() {
-    // Given we create a new process instance
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("testProcess");
-    // Then it should be active
-    assertThat(processInstance).isActive();
-    // And it should be the only instance
-    assertThat(processInstanceQuery().count()).isEqualTo(1);
-    // And there should exist just a single task within that process instance
-    assertThat(task(processInstance)).isNotNull();
+  static ControllableThread activeThread;
 
-    // When we complete that task
-    complete(task(processInstance));
-    // Then the process instance should be ended
-    assertThat(processInstance).isEnded();
+  class SetTaskVariablesThread extends ControllableThread {
+
+    OptimisticLockingException optimisticLockingException;
+    Exception exception;
+
+    protected Object variableValue;
+    protected String taskId;
+    protected String variableName;
+
+    public SetTaskVariablesThread(String taskId, String variableName, Object variableValue) {
+      this.taskId = taskId;
+      this.variableName = variableName;
+      this.variableValue = variableValue;
+    }
+
+    public synchronized void startAndWaitUntilControlIsReturned() {
+      activeThread = this;
+      super.startAndWaitUntilControlIsReturned();
+    }
+
+    public void run() {
+      try {
+        processEngineConfiguration
+          .getCommandExecutorTxRequired()
+          .execute(new ControlledCommand(activeThread, new SetTaskVariablesCmd(taskId, Collections.singletonMap(variableName, variableValue), false)));
+
+      } catch (OptimisticLockingException e) {
+        this.optimisticLockingException = e;
+      } catch (Exception e) {
+        this.exception = e;
+      }
+      LOG.debug(getName()+" ends");
+    }
+  }
+
+  @Deployment(resources="testProcess.bpmn")
+  public void testConcurrentVariableCreate() {
+
+    runtimeService.startProcessInstanceByKey("testProcess", Collections.<String, Object>singletonMap("varName1", "someValue"));
+
+    String variableName = "varName";
+    String taskId = taskService.createTaskQuery().singleResult().getId();
+
+    SetTaskVariablesThread thread1 = new SetTaskVariablesThread(taskId, variableName, "someString");
+    thread1.startAndWaitUntilControlIsReturned();
+
+    // this should fail with integrity constraint violation
+    SetTaskVariablesThread thread2 = new SetTaskVariablesThread(taskId, variableName, "someString");
+    thread2.startAndWaitUntilControlIsReturned();
+
+    thread1.proceedAndWaitTillDone();
+    assertNull(thread1.exception);
+    assertNull(thread1.optimisticLockingException);
+
+    thread2.proceedAndWaitTillDone();
+    assertNull(thread2.exception);
+    assertNotNull(thread2.optimisticLockingException);
+
+    // should not fail with FK violation because one of the variables is not deleted.
+    taskService.complete(taskId);
   }
 
 }
